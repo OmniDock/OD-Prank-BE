4460f4d6090783bb375cd90fe18188a74e2f4df2
Sebastian R. <63642526+SebRoe@users.noreply.github.com>
2025-09-09 17:07:42 +0200

    Add CallLog to models initialization
    - Included `CallLog` in the `__init__.py` file of the models directory to ensure it is properly imported and accessible throughout the application. This change enhances the model's visibility and usability within the codebase.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,9 +3,10 @@ from .scenario import Scenario
 from .voice_line import VoiceLine
 from .voice_line_audio import VoiceLineAudio
 from .blacklist import Blacklist
+from .call_log import CallLog
 import sqlalchemy_continuum
 
 # Initialize continuum (Similar to Django's django-simple-history)
 sqlalchemy_continuum.make_versioned(user_cls=None)
 
-__all__ = ["Base", "Scenario", "VoiceLine", "VoiceLineAudio", "Blacklist"]
+__all__ = ["Base", "Scenario", "VoiceLine", "VoiceLineAudio", "Blacklist", "CallLog"]
